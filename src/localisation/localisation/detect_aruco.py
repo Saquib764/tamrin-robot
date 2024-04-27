404044f15e7b14249baa680073bb47ae8d2dc8ae
Saquiub <saquibvswild@gmail.com>
2024-04-27 11:57:39 +0530

    transformation works

--- src/localisation/localisation/detect_aruco.py
+++ src/localisation/localisation/detect_aruco.py
@@ -1,10 +1,19 @@
 import rclpy
 from rclpy.node import Node
-from sensor_msgs.msg import Image
+from sensor_msgs.msg import Image, CameraInfo
+from geometry_msgs.msg import TransformStamped, PoseStamped
 from cv_bridge import CvBridge
 import cv2
 import cv2.aruco as aruco
-# from PIL import Image
+import numpy as np
+
+import tf2_ros
+from tf2_ros import Buffer, TransformListener
+import tf_transformations
+from tf_transformations import quaternion_from_euler, quaternion_from_matrix, translation_from_matrix
+from tf_transformations import translation_matrix, quaternion_matrix, concatenate_matrices
+
+
 
 class ArUcoDetector(Node):
     def __init__(self):
@@ -14,9 +23,32 @@ class ArUcoDetector(Node):
             '/my_robot/camera/image_color',  # Replace with your camera topic
             self.image_callback, 10)
         self.subscription  # prevent unused variable warning
+
+        self.camera_info_subscription = self.create_subscription(
+            CameraInfo,
+            '/my_robot/camera/camera_info',  # Replace with your camera info topic
+            self.camera_info_callback, 10)
         self.cv_bridge = CvBridge()
 
+        self.camera_matrix = None
+        self.dist_coeffs = None
+        self.marker_length = 0.25  # Marker length in meters
+
+        # Transformations
+        self.tf_buffer = Buffer()
+        self.tf_listener = TransformListener(self.tf_buffer, self)
+        self.br = tf2_ros.TransformBroadcaster(self)
+
+
+    def camera_info_callback(self, msg):
+        # Assuming no distortion
+        self.camera_matrix = np.array(msg.k).reshape([3, 3])
+        self.dist_coeffs = np.array(msg.d)
+
     def image_callback(self, msg):
+        if self.camera_matrix is None:
+            self.get_logger().error("Camera matrix not set")
+            return
         try:
             cv_image = self.cv_bridge.imgmsg_to_cv2(msg, "bgr8")
         except Exception as e:
@@ -34,13 +66,67 @@ class ArUcoDetector(Node):
 
         corners, ids, rejectedImgPoints = detector.detectMarkers(gray)
 
-        print(corners, ids)
+        # print(corners, ids)
         # im = Image.fromarray(cv_image)
         # im.save("aruco_input.png")
         # cv2.imwrite("aruco_input.png", cv_image)
         # Draw detected markers
         if ids is not None:
             cv_image = aruco.drawDetectedMarkers(cv_image, corners, ids)
+            rvecs, tvecs, _objPoints = aruco.estimatePoseSingleMarkers(
+                corners, self.marker_length, self.camera_matrix, self.dist_coeffs
+            )
+
+            # Draw the axes of the detected markers
+            cv2.drawFrameAxes(cv_image, self.camera_matrix, self.dist_coeffs, rvecs, tvecs, 0.1)
+
+            for rvec, tvec in zip(rvecs, tvecs):
+                now = rclpy.time.Time()
+
+                R, _ = cv2.Rodrigues(rvec[0])
+                quaternion = quaternion_from_matrix(np.r_[np.c_[R, [0, 0, 0]], [[0, 0, 0, 1]]])
+
+                # Create a 4x4 transformation matrix
+                transform_camera_marker_matrix = concatenate_matrices(
+                    translation_matrix(tvec[0]),
+                    quaternion_matrix(quaternion)
+                )
+
+                try:
+
+                    transform_base_camera = self.tf_buffer.lookup_transform('base_link', 'camera', now)
+                    translation = [
+                        transform_base_camera.transform.translation.x,
+                        transform_base_camera.transform.translation.y,
+                        transform_base_camera.transform.translation.z
+                    ]
+                    rotation = [
+                        transform_base_camera.transform.rotation.x,
+                        transform_base_camera.transform.rotation.y,
+                        transform_base_camera.transform.rotation.z,
+                        transform_base_camera.transform.rotation.w
+                    ]
+
+                    # Create a 4x4 transformation matrix
+                    transform_base_camera_matrix = concatenate_matrices(
+                        translation_matrix(translation),
+                        quaternion_matrix(rotation)
+                    )
+
+                    transform_base_marker_matrix = transform_base_camera_matrix.dot(transform_camera_marker_matrix)
+
+
+                    quaternion = quaternion_from_matrix(transform_base_marker_matrix)
+                    translation = translation_from_matrix(transform_base_marker_matrix)
+                    print("Transform Base to Marker: ", translation, quaternion)
+
+
+                    # transformed_pose = self.tf_buffer.transform(marker_pose, 'shaft_link', rclpy.duration.Duration(seconds=1))
+                    # self.get_logger().info("Transformed Marker Pose: %s" % transformed_pose)
+                    
+                except Exception as e:
+                    self.get_logger().info(f'Failed to transform marker pose: {str(e)}')
+
 
         # Display the image with markers
         cv2.imshow("ArUco Marker Detection", cv_image)
--- src/tamrin_robot/launch/webots_main.launch.py
+++ src/tamrin_robot/launch/webots_main.launch.py
@@ -4,11 +4,15 @@ from launch import LaunchDescription
 from ament_index_python.packages import get_package_share_directory
 from webots_ros2_driver.webots_launcher import WebotsLauncher, Ros2SupervisorLauncher
 from webots_ros2_driver.webots_controller import WebotsController
+from launch_ros.actions import Node
 
 
 def generate_launch_description():
     pkg_path = os.path.join(get_package_share_directory('tamrin_robot'))
-    robot_description_path = os.path.join(pkg_path, 'description', 'webots_robot.urdf')
+    robot_description_path = os.path.join(pkg_path, 'description', 'my_robot.webots.urdf')
+    robot_description_text = None
+    with open(robot_description_path, 'r') as file:
+        robot_description_text = file.read()
 
     webots = WebotsLauncher(
         world=os.path.join(pkg_path, 'worlds', 'webots_world_1.wbt')
@@ -20,9 +24,18 @@ def generate_launch_description():
     my_robot_driver = WebotsController(
         robot_name='my_robot',
         parameters=[
-            {'robot_description': robot_description_path},
+            {
+                'robot_description': robot_description_path
+            },
         ],
         respawn=True
+    )
+
+    state_publisher = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        name='robot_state_publisher',
+        parameters=[{'robot_description': robot_description_text}],
     )
 
 
@@ -30,6 +43,7 @@ def generate_launch_description():
         webots,
         ros2_supervisor,
         my_robot_driver,
+        state_publisher,
         launch.actions.RegisterEventHandler(
             event_handler=launch.event_handlers.OnProcessExit(
                 target_action=webots,
